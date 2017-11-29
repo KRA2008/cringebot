45f4975980eba18f7673c6775acacd89ea131e64
Kevin Anderson <kraatw2008@yahoo.com>
2017-11-28 23:29:26 -0600

    *cringe*

---
+++ Cringebot/Cringebot/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Cringebot
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
