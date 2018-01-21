5c1acacf4b2a682371e2fad59d6046f18dd48b00
Kevin Anderson <kraatw2008@yahoo.com>
2018-01-21 17:10:39 -0600

    removing ability to limit list visibility on iOS because the list flashes all its contents as you search, which defeats the purpose entirely. TODO: come back with a fix for that

---
+++ Cringebot/Cringebot.iOS/CustomRenderers/DeselectListViewRenderer.cs
@@ -0,0 +1,34 @@
+﻿using Xamarin.Forms.Platform.iOS;
+using Xamarin.Forms;
+using Cringebot.Page.CustomElements;
+using Cringebot.iOS.CustomRenderers;
+
+[assembly: ExportRenderer(typeof(DeselectListView), typeof(DeselectListViewRenderer))]
+namespace Cringebot.iOS.CustomRenderers
+{
+    public class DeselectListViewRenderer : ListViewRenderer
+    {
+        private bool _isConfigured;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<ListView> e)
+        {
+            base.OnElementChanged(e);
+
+            if(e.OldElement != null)
+            {
+                e.OldElement.ItemAppearing -= ItemAppearing;
+            }
+
+            if (e.NewElement != null && !_isConfigured)
+            {
+                _isConfigured = true;
+                e.NewElement.ItemAppearing += ItemAppearing;
+            }
+        }
+
+        private void ItemAppearing(object sender, ItemVisibilityEventArgs e)
+        {
+            var what = e.Item;
+        }
+    }
+}
