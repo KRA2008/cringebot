ae4c801e723ce60f7b6df7a4a587f67b0829c03f
Kevin Anderson <kraatw2008@yahoo.com>
2018-02-09 21:27:47 -0600

    adding an android splash screen

--- Cringebot/Cringebot.Android/MainActivity.cs
+++ Cringebot/Cringebot.Android/MainActivity.cs
@@ -4,7 +4,7 @@ using Android.OS;
 
 namespace Cringebot.Droid
 {
-    [Activity(Label = "Cringebot", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Cringebot", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
---
+++ Cringebot/Cringebot.Android/SplashActivity.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+
+namespace Cringebot.Droid
+{
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnResume()
+        {
+            base.OnResume();
+            //Task.Run(() =>
+            //{
+                StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+            //});
+        }
+
+        public override void OnBackPressed() { }
+    }
+}
