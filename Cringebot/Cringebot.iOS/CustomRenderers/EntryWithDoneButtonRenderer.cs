1ae74f2060960b87bd2febaaaa4047d0e946143f
Kevin Anderson <kraatw2008@yahoo.com>
2018-03-03 13:27:50 -0600

    adding renderer for entries that puts a done button in place of return button on ios

---
+++ Cringebot/Cringebot.iOS/CustomRenderers/EntryWithDoneButtonRenderer.cs
@@ -0,0 +1,18 @@
+﻿using Cringebot.iOS.CustomRenderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(EntryWithDoneButtonRenderer))]
+namespace Cringebot.iOS.CustomRenderers
+{
+    public class EntryWithDoneButtonRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null && e.NewElement != null)
+                Control.ReturnKeyType = UIReturnKeyType.Done;
+        }
+    }
+}
